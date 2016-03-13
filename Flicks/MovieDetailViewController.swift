755cd043c2ddea9453df039260c1765a2ecf17cb
Hoang Nguyen <nguyenminhhoang210891@gmail.com>
2016-03-13 12:44:00 +0700

    add toggle movie overview feature

--- Flicks/MovieDetailViewController.swift
+++ Flicks/MovieDetailViewController.swift
@@ -13,29 +13,27 @@ class MovieDetailViewController: UIViewController {
     // MARK: - Attributes
     
     @IBOutlet weak var infoView: UIView!
-    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
     @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var overviewScrollView: UIScrollView!
+    @IBOutlet weak var toggleInfoButton: UIButton!
+    
+    var infoToggled = false
     
     var movie: NSDictionary!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
+        //scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
         
-        print(infoView.frame.origin.y)
-        print(infoView.frame.origin.x)
-        print(infoView.frame.size.height)
-
         // Do any additional setup after loading the view.
         let title = movie["title"] as! String
         titleLabel.text = title
         
         let overview = movie["overview"] as! String
         overviewLabel.text = overview
-        overviewLabel.sizeToFit()
         
         let baseUrl = "https://image.tmdb.org/t/p/w342"
         
@@ -50,6 +48,42 @@ class MovieDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func upClick(sender: AnyObject) {
+        if(!infoToggled) {
+            UIView.animateWithDuration(0.3) { () -> Void in
+                self.toggleInfoButton.setImage(UIImage(named: "arrowdown"), forState: .Normal)
+                //let originOverviewLabelY = self.overviewLabel.frame.origin.y + self.overviewLabel.frame.size.height
+                self.infoView.frame.origin.y = 180
+                var newFrame = self.infoView.frame
+                newFrame.size.height = 568 - 180
+                self.infoView.frame = newFrame
+                self.overviewLabel.numberOfLines = 0
+                self.overviewLabel.sizeToFit()
+                //self.overviewLabel.frame.size.height = 300
+                self.overviewScrollView.frame.size.height += 180
+                //self.overviewScrollView.contentSize = CGSize(width: self.overviewScrollView.frame.size.width, height: self.overviewLabel.frame.size.height)
+                // print(originOverviewLabelY)
+                // print(self.overviewScrollView.frame.origin.y)
+            }
+        } else {
+            UIView.animateWithDuration(0.3) { () -> Void in
+                self.toggleInfoButton.setImage(UIImage(named: "arrowup"), forState: .Normal)
+                //let originOverviewLabelY = self.overviewLabel.frame.origin.y + self.overviewLabel.frame.size.height
+                self.infoView.frame.origin.y = 356
+                var newFrame = self.infoView.frame
+                newFrame.size.height = 167
+                self.infoView.frame = newFrame
+                self.overviewLabel.numberOfLines = 3
+                self.overviewLabel.sizeToFit()
+                //self.overviewLabel.frame.size.height = 300
+                self.overviewScrollView.frame.size.height = 86
+                //self.overviewScrollView.contentSize = CGSize(width: self.overviewScrollView.frame.size.width, height: self.overviewLabel.frame.size.height)
+                // print(originOverviewLabelY)
+                // print(self.overviewScrollView.frame.origin.y)
+            }
+        }
+        infoToggled = !infoToggled
+    }
 
     /*
     // MARK: - Navigation
